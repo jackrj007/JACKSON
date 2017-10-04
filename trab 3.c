e7b7fea49912b7b229e8ae597319c44b27db4deb
jackrj007 <32281033+jackrj007@users.noreply.github.com>
2017-10-04 00:59:19 -0300

    Update trab 3.c

--- trab 3.c
+++ trab 3.c
@@ -3,53 +3,44 @@
 
 int main()
 {
-	int num, num2, p=1;
+	int num1, num2, opc=1;
 	
 	setlocale(LC_ALL, "Portuguese");
 	
 	printf("\nOlá, meu nome JACKSON DA ROCHA NASCIMENTO Eu vou calcular a soma dos numeros não primos\n");
 	
-	while (p<=1) 
-	{	
+	while (opc<=1) 
+	{
 	
 		do
 		{
-			printf("\n\nEntre com o primeiro número: ");
-			scanf("%d", &num);		
+			printf("\nEntre com o primeiro número: ");
+			scanf("%d", &num1);
+		
+	
+		printf("\nEntre com o segundo número: ");
+			scanf("%d", &num2);
         }
-		while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
-		
-		if ( ((num%2 > 0) || (num%3 > 0) || (num%5 > 0) || (num%7 > 0)))
-		printf("\n%d não é um número válido, pois é primo.", num);
-		
-		else
-        
-		do
-        {
-	        do
-	        {
-				printf("\nEntre com o segundo número: ");
-				scanf("%d", &num2);
-			}
-			while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
-		
-			if ( ((num2==1) || (num2%2 == 0) || (num2%3 == 0) || (num2%5 == 0) || (num2%7 == 0)) && !((num2==2) || (num2==3) || (num2==5) || (num2==7)) )
-			printf("\n%d não é um número válido, pois é primo.", num2);
-			
-			else
-			printf("\n%d + %d = %d", num, num2, num + num2);   
-		}
-		while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
-        
-		printf("\n\nDeseja continuar (1-Sim / 2-Não) ? ");
-		scanf("%d", &p);
+		while(num1,num2<=0);
+
+		if ( ((num1==1||num2==1) || (num1%2 == 0||num2%2 == 0) || (num1%3 == 0||num2%3 == 0) || (num1%3 == 0||num2%5 == 0) || (num1%7 == 0||num2%7 == 0)) && !((num1==2||num2==2) || (num1==3||num2==3) || (num1==5||num2==5) || (num1==7||num2==7)) )
+
+
+          
+	printf("%d + %d = %d", num1, num2, num1 + num2);	
+	
+	else
+	
+	printf("\nUm ou ambos os números digitados é primo. Tente novamente.");
+
+		printf("\nDeseja continuar (1-Sim / 2-Não) ? ");
+		scanf("%d", &opc);
 	}
 	
-	if (p=2)
+	if (opc=2) 
 	
-	printf("\nObrigado pelo teste até mais\n\n");
+	printf("\nObrigado, pelo teste até mais\n\n");
 
 	system("pause");
 	return 0;
 }
-
