41565bb3e9b3ad81bd379d2f57bf4ad14aa38bb7
jackrj007 <32281033+jackrj007@users.noreply.github.com>
2017-10-04 00:45:24 -0300

    Update trab 3.c

--- trab 3.c
+++ trab 3.c
@@ -7,20 +7,20 @@ int main()
 	
 	setlocale(LC_ALL, "Portuguese");
 	
-	printf("\nOl·, meu nome JACKSON DA ROCHA NASCIMENTO Eu vou calcular a soma dos numeros n„o primos\n");
+	printf("\nOl√°, meu nome JACKSON DA ROCHA NASCIMENTO Eu vou calcular a soma dos numeros n√£o primos\n");
 	
 	while (p<=1) 
 	{	
 	
 		do
 		{
-			printf("\n\nEntre com o primeiro n˙mero: ");
+			printf("\n\nEntre com o primeiro n√∫mero: ");
 			scanf("%d", &num);		
         }
 		while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
 		
 		if ( ((num%2 > 0) || (num%3 > 0) || (num%5 > 0) || (num%7 > 0)))
-		printf("\n%d n„o È um n˙mero v·lido, pois È primo.", num);
+		printf("\n%d n√£o √© um n√∫mero v√°lido, pois √© primo.", num);
 		
 		else
         
@@ -28,26 +28,26 @@ int main()
         {
 	        do
 	        {
-				printf("\nEntre com o segundo n˙mero: ");
+				printf("\nEntre com o segundo n√∫mero: ");
 				scanf("%d", &num2);
 			}
 			while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
 		
 			if ( ((num2==1) || (num2%2 == 0) || (num2%3 == 0) || (num2%5 == 0) || (num2%7 == 0)) && !((num2==2) || (num2==3) || (num2==5) || (num2==7)) )
-			printf("\n%d n„o È um n˙mero v·lido, pois n„o È primo.", num2);
+			printf("\n%d n√£o √© um n√∫mero v√°lido, pois √© primo.", num2);
 			
 			else
 			printf("\n%d + %d = %d", num, num2, num + num2);   
 		}
 		while((num<=0) && ((num==1) || (num%2>0) || (num%3>0) || (num%5>0) || (num%7>0)));
         
-		printf("\n\nDeseja continuar (1-Sim / 2-N„o) ? ");
+		printf("\n\nDeseja continuar (1-Sim / 2-N√£o) ? ");
 		scanf("%d", &p);
 	}
 	
 	if (p=2)
 	
-	printf("\nObrigado, atÈ a prÛxima!\n\n");
+	printf("\nObrigado, at√© a pr√≥xima!\n\n");
 
 	system("pause");
 	return 0;
